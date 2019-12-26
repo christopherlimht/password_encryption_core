a22a916cec08bdc4954dbe89ccbe9c9af06ee7d5
Christopher Lim <christopher.lim@singtel.com>
2019-12-26 17:29:59 +0800

    working aes encryption

---
+++ crypto_core/crypto.js
@@ -0,0 +1,43 @@
+const forge = require('node-forge')
+
+function generateKey(byteSize,password){
+    let numIteration = 12;
+    let salt = forge.random.getBytesSync(byteSize);
+    let key = forge.pkcs5.pbkdf2(password,salt,numIteration,byteSize/8);
+    let iv = forge.random.getBytesSync(byteSize/8);
+    return {key:key,iv:iv};
+}
+
+function encryptAES(iv,key,object){
+    let cipher = forge.cipher.createCipher('AES-CBC',key);
+    cipher.start({iv:iv});
+    cipher.update(forge.util.createBuffer(object));
+    let result = cipher.finish();
+    if(result){
+        console.log("encryption successful");
+    }else{
+        console.log("encryption failed");
+    }
+    let encrypted = cipher.output;
+    return encrypted;
+}
+
+function decryptAES(iv,key,object){
+    let decipher = forge.cipher.createDecipher('AES-CBC',key);
+    decipher.start({iv:iv});
+    decipher.update(object);
+    let result = decipher.finish();
+    if(result){
+        console.log("decryption successful");
+    }else{
+        console.log("decryption failed");
+    }
+    let plainOutput = decipher.output;
+    return plainOutput;
+}
+
+module.exports = {
+    generateKey:generateKey,
+    encryptAES:encryptAES,
+    decryptAES:decryptAES
+}
---
+++ index.js
@@ -0,0 +1,45 @@
+const program = require('commander')
+const process = require('process');
+const crypto = require('./crypto_core/crypto.js')
+const debug = true;
+let cmdValue = ''
+program.command('encrypt')
+    .alias('e')
+    .option('-k, --key <type>')
+    .option('-o, --object <type>')
+    .action(function(){
+        cmdValue = 'encrypt'
+    })
+
+program.command('decrypt')
+    .alias('d')
+    .option('-k, --key <type>')
+    .option('-o, --object <type>')
+    .action(function(){
+        cmdValue = 'decrypt'
+    })
+
+program.parse(process.argv);
+
+if(cmdValue == 'encrypt'){
+    console.log("In encryption mode");
+    var pair = crypto.generateKey(128,program.commands[0].key);
+    var payload = encrypt(pair.iv,pair.key,program.commands[0].object);
+    console.log(`This is the encrypted payload: ${payload.toHex()}`);
+    var plainText = decrypt(pair.iv,pair.key,payload);
+    console.log(`This is the decrypted plaintext: ${plainText.toString('utf-8')}`);
+}else if(cmdValue == 'decrypt'){
+    console.log("In decryption mode");
+}else{
+    console.log("err")
+}
+
+function encrypt(iv,key,obj){
+    var cipher = crypto.encryptAES(iv,key,obj);
+    return cipher;
+}
+
+function decrypt(iv,key,obj){
+    var cipher = crypto.decryptAES(iv,key,obj);
+    return cipher;
+}
